707e8656dda47cffba70263b54fdb580ea9d956f
Julio Martinez <julio.martinez@iudigital.edu.co>
2024-10-11 02:41:35 +0200

    :lock: cors

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ const { mongoConnect } = require('./databases/config')
 mongoConnect()
 
 // middlewares
-app.use(cors({
-    origin: '*',
-    methods : ['GET', 'POST', 'PUT', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'token', 'Authorization'],
-    credentials: true
-}))
+app.use(
+    cors({
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Permite métodos
+        allowedHeaders: ['Content-Type', 'Authorization', 'token'], // Cabeceras permitidas
+        credentials: true
+    })
+)
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
