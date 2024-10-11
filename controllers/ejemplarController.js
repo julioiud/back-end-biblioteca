794621065d25bd3c78588845931f21a5f8e6bcbc
Julio Martinez <julio.martinez@iudigital.edu.co>
2024-10-11 04:38:06 +0200

    ejemplares

--- controllers/ejemplarController.js
+++ controllers/ejemplarController.js
@@ -4,6 +4,10 @@ const Ejemplar = require('../models/ejemplar')
 const consultarEjemplares = async (req = request, res = response) => {
     try {
         const ejemplares = await Ejemplar.find()
+        .populate({
+            path: 'libro',
+            select: '_id titulo'
+        })
         return res.json(ejemplares)
     } catch(e) {
         console.log(e)
